ab466778abcf35cbf0e5dba07454b71e387f74a9
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-08 18:36:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -31,7 +31,7 @@ end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash[:winter].each do |holiday, supplies|
-    holiday.push(supply)
+    supplies.push(supply)
   end 
   holiday_hash
   # holiday_hash is identical to the one above
