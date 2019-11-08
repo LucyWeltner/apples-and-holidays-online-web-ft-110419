b11146f87d365b5bdb0ed71fefb654c1d327fc7f
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-08 18:48:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season_string}:"
     holidays.each do |holiday, supplies|
       string_supplies = supplies.join(", ")
-      puts.chomp("#{holiday}: #{string_supplies}")
+      puts "#{holiday}: #{string_supplies}"
     end
   end
   holiday_hash
