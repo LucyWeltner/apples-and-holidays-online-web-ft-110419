f87ac1ced00010b34ce9a480a91f54c9358181a0
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-08 18:38:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,7 +20,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # return the second element in the 4th of July array
   holiday_hash.each do |season, holidays|
     if season == :summer
-      holidays do |holiday, supplies|
+      holidays.each do |holiday, supplies|
         if holiday == :fourth_of_july
           return supplies[1]
         end 
